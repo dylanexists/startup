2a6afa1665c5681ed5355e755ea9f6cf5cbdb686
Dylan Steenhoek <dylans18@byu.edu>
2026-07-10 08:07:10 -0600

    Create initial app component

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './output-r1.css';
+import './app.css';
+
+export default function App() {
+  return <div className="body bg-dark text-light">App will display here</div>;
+}
