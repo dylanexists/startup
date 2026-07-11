b5412d9ca5d25883df944236d8339be16bf5f5a7
Dylan Steenhoek <dylans18@byu.edu>
2026-07-10 19:32:08 -0600

    Convert login page to a React component

--- src/app.jsx
+++ src/app.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, NavLink, Link } from 'react-router-dom';
 import './output-r1.css';
 import './app.css';
 import { FindApartment } from './find-apartment/find-apartment';
 import { Login } from './login/login';
 import { RegisterUser } from './register-user/register-user';
 import { AdminDashboard } from './admin-dashboard/admin-dashboard';
+import { UserDashboard } from './user-dashboard/user-dashboard';
 
 export default function App() {
   return (
-    
+    <BrowserRouter>
     <div className="body bg-dark text-light">
-        <header class="flex items-center px-8 py-4">
-            <a href="index.html" class="block shrink-0">
-            <img src="/free-house-icon.png" alt="Logo" class="h-auto max-h-[50px] w-auto object-contain"></img>
-            </a>
-            <h1 class="text-4xl font-bold tracking-tight text-gray-900">
+        <header className="flex items-center px-8 py-4">
+            <Link to="/" className="block shrink-0">
+            <img src="/free-house-icon.png" alt="Logo" className="h-auto max-h-[50px] w-auto object-contain"></img>
+            </Link>
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900">
                 RentItBest</h1>
-            <nav class="ml-auto">
-                <menu class="flex justify-end items-center gap-4">
-                    <li><NavLink to="">Login</NavLink></li>
-                    <li><NavLink to="find-apartment">Find an Apartment</NavLink></li>
+            <nav className="ml-auto">
+                <menu className="flex justify-end items-center gap-4">
+                    <li><NavLink to="/">Login</NavLink></li>
+                    <li><NavLink to="/find-apartment">Find an Apartment</NavLink></li>
                 </menu>
             </nav>
         </header>
 
         <Routes>
-            <Route path='/' element={<Login />} exact />
+            <Route path='/' element={<Login />} />
             <Route path='/find-apartment' element={<FindApartment />} />
             <Route path='/register-user' element={<RegisterUser />} />
             <Route path='/admin-dashboard' element={<AdminDashboard />} />
@@ -40,6 +42,7 @@ export default function App() {
             <a href="https://github.com/dylanexists/startup">GitHub</a>
         </footer>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,39 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
-    </main>
+    <main className="flex flex-col justify-center items-center min-h-screen py-12">
+            <h1 className="text-5xl font-bold tracking-tight text-gray-900">
+                Welcome to RentItBest</h1>
+            <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+                The Best Rental Solution for Tenants and Landlords Alike</h2>
+            <img src="https://images.unsplash.com/photo-1613490493576-7fde63acd811?w=800" alt="Fancy Apartment" className="max-w-xs m-[10px]"></img>
+            <section className="p-6 bg-white border border-gray-300 rounded-lg shadow-sm space-y-6">
+                <form method="get" className="space-y-4">
+                    <h4 className="text-center text-xl font-bold">Login</h4>
+                    <div>
+                        <span className="text-left">Email</span>
+                        <input className="w-full border border-gray-300 rounded px-3 py-2"
+                        type="text" placeholder="your@email.com" required/>
+                    </div>
+                    <div>
+                        <span className="text-left">Password</span>
+                        <input className="w-full border border-gray-300 rounded px-3 py-2"
+                        type="password" placeholder="password" required/>
+                    </div>
+                    <div  className="text-center">
+                    <button type="submit" formaction="user-dashboard.html" className="bg-[#0f417a] hover:bg-[#0a2f58] text-white font-semibold py-2 px-6 rounded-md shadow-sm m-2">
+                        Login as User</button>
+                    <button type="submit" formaction="admin-dashboard.html" className="bg-[#0f417a] hover:bg-[#0a2f58] text-white font-semibold py-2 px-6 rounded-md shadow-sm m-2">
+                        Login as Admin</button>
+                    </div>
+                </form>
+                <div className="text-center">
+                    <h4 className="m-4">Don't have an account/apartment?</h4>
+                    <a href="find-apartment.html" className="bg-[#f2f5f8] hover:bg-[#f5f5f8] font-semibold py-2 px-6 rounded-md shadow-sm"
+                            >Come find an apartment!</a>
+                </div>
+                <br></br>
+            </section>
+        </main>
   );
 }
