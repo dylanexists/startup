96603545447f4a5b50ba2ed318c3382d8adebdde
Dylan Steenhoek <dylans18@byu.edu>
2026-08-05 17:40:36 -0600

    Install WS in front and back end, and confirm ws testing

--- service/peerProxy.js
+++ service/peerProxy.js
@@ -2,6 +2,7 @@ const { WebSocketServer, WebSocket } = require('ws');
 
 function peerProxy(httpServer) {
   // Create a websocket object
+  console.log("Setting up WS server...")
   const socketServer = new WebSocketServer({ server: httpServer });
 
   // Map of userId to socket instance
@@ -9,6 +10,7 @@ function peerProxy(httpServer) {
 
   socketServer.on('connection', (socket) => {
     socket.isAlive = true;
+    console.log("eyyyy")
 
     // Forward messages to everyone except the sender
     socket.on('message', function message(rawData) {
@@ -19,16 +21,26 @@ function peerProxy(httpServer) {
             if (socket.userId && connections.get(socket.userId) === socket) {
                 connections.delete(socket.userId); //get rid of previous authentication of same socket, if exists
             }
-
             socket.userId = data.userId
             connections.set(data.userId, socket)
         }
 
-        if (data.type === 'user_notification') {
-            const recipientClient = connections.get(data.recipientId)
+        if (data.type === 'system') {
+            socketServer.clients.forEach((client) => {
+                if (client !== socket && client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify(data));
+                }
+            });
+        }
 
-            if (recipientClient && recipientClient.readyState === WebSocket.OPEN) {
-                recipientClient.send(data.content)
+        if (data.type === 'user_notification') {
+            const recipientId = data.recipientId
+
+            if (recipientId) {
+                const recipientClient = connections.get(recipientId)
+                if (recipientClient && recipientClient.readyState === WebSocket.OPEN) {
+                    recipientClient.send(JSON.stringify(data))
+                }
             }
         }
       } catch (err) {
