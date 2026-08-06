284dc3077e4de998e6afa2bf58508ed43b7ab617
Dylan Steenhoek <dylans18@byu.edu>
2026-08-05 20:27:02 -0600

    Implement WS notifications for users when an admin sends a technician

--- src/notification.jsx
+++ src/notification.jsx
@@ -1,22 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { DashEvent, DashNotifier } from './dashNotifier';
 import './app.css';
 
 export function Notification() {
 
-  const [event, setEvent] = React.useState();
+  const [event, setEvent] = React.useState()
+  const [animationKey, setAnimationKey] = useState(1)
+  const timerRef = useRef(null)
 
   React.useEffect(() => {
     DashNotifier.addHandler(handleDashEvent);
 
     return () => {
-      DashNotifier.removeHandler(handleDashEvent);
+      DashNotifier.removeHandler(handleDashEvent)
+      if (timerRef.current) clearTimeout(timerRef.current)
     };
-  });
+  }, []);
 
   function handleDashEvent(event) {
+    if (event.type !== DashEvent.UserNoti) return;
+
     console.log("Successful event:", event)
     setEvent(event);
+
+    setAnimationKey((prev) => prev + 1)
+
+    // Set notifications to only last 10 seconds
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+
+    timerRef.current = setTimeout(() => {
+      setEvent(undefined);
+    }, 10000);
   }
 
   if (!event) return null
@@ -25,13 +41,12 @@ export function Notification() {
         <div className="notification-content">
             <div className="flex justify-between items-center w-full">
                 <h3 className="font-bold">Notification</h3>
-                <button 
-                    className="text-gray-900 hover:text-[#9e9e9e] font-bold text-right">
-                    ✕ Close
-                </button>
             </div>
             <h4>Placeholder</h4>
             <p>la la la la la la la laaa</p>
+        </div>
+        <div className="w-full bg-gray-200 h-1">
+            <div key={animationKey} className="progress-bar" />
         </div>
     </div>
   )
